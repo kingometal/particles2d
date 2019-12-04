5a3304afc08b46dcce35d824170772f65d27b320
Igor Beljakov <igor.beljakov@gmx.de>
2019-12-04 11:08:51 +0100

    Remove old ParticlesView based on XWindows

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,6 @@
 #include "Config.h"
 #include "Particles.h"
 #include "ParticlesViewSdlAdapter.h"
-#include "ParticlesView.h"
 #include "Presenter.h"
 #include "UserInput.h"
 
@@ -10,7 +9,6 @@ int main(void)
     Presenter* presenter = new Presenter();
     Config* parameters = new Config();
     ParticlesViewInterface* view = new ParticlesViewSdlAdapter(*presenter, parameters->WindowSideSize, parameters->WindowSideSize, parameters->MaxFPS);
-//    ParticlesViewInterface* view = new ParticlesView(parameters->WindowSideSize);
     UserInput* userInput = new UserInput();
 
     Particles Ps(*view, *userInput, *parameters);
